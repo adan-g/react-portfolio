f785b1fcaa210058abff28e70f9b630cc151e26f
Adan Gimenez <gimenezadan1@gmail.com>
2023-12-15 10:09:46 -0600

    email updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
 
   return (
     <section id={'contact'} className='mx-7'>
+      <Modal open={openModal} onClose={() => setOpenModal(false)} />
       <h1 className='lg:text-[9rem] text-7xl text-center text-black12 font-semibold my-24' style={{ '-webkit-text-stroke-width': '2px', '-webkit-text-stroke-color': '#93B1A6', 'text-shadow': '#93B1A6 1px 0 20px' }}>
         CONTACT
       </h1>
@@ -21,7 +22,6 @@ const Contact = () => {
           <BiLogoGmail />
         </a>
       </nav>
-      <Modal open={openModal} onClose={() => setOpenModal(false)} />
     </section>
   )
 }
--- src/components/modal/ModalContact.jsx
+++ src/components/modal/ModalContact.jsx
@@ -1,19 +1,27 @@
+import { RiCloseCircleLine } from "react-icons/ri";
+import { RiSendPlaneFill } from "react-icons/ri";
 
 const ModalContact = ({ open, onClose }) => {
   if (!open) return null
   return (
-    <div className='bg-blue-400 w-full h-full'>
-      <header className="flex justify-between mb-4">
-        <h1 className="font-bold">Send a email</h1>
-        <p className="cursor-pointer" onClick={onClose}>X</p>
-      </header>
-      <form className="flex flex-col">
-        <input className="w-1/2 mb-4" type="email" placeholder="Type your email" />
-        <input className="w-1/2 mb-4" type="subject" placeholder="type your subject" />
-        <textarea className="w-1/2 mb-4" />
-        <button className="bg-slate-500 w-1/2">Send</button>
-      </form>
-    </div>
+    <section className='w-full h-full top-0 left-0 fixed bg-[rgb(0,0,0,0.6)] flex justify-center items-center p-4'>
+      <div className="bg-green3d w-1/2 p-4">
+        <header className="flex justify-between mb-4 text-slate-200 items-center">
+          <h1 className="font-extrabold text-2xl">Contact Form</h1>
+          <p className="cursor-pointer text-xl" onClick={onClose}>
+            <RiCloseCircleLine />
+          </p>
+        </header>
+        <form className="flex flex-col items-center">
+          <input className="w-full mb-4 p-1 bg-greenA6 placeholder:text-slate-200" type="email" placeholder="Type your email" />
+          <input className="w-full mb-4 p-1 bg-greenA6 placeholder:text-slate-200" type="subject" placeholder="Type your subject" />
+          <textarea className="w-full mb-4 h-40 bg-greenA6 placeholder:text-slate-200" placeholder="What is your message?..."/>
+          <button className="bg-black12 text-slate-200 w-1/2 flex items-center justify-center p-4">
+            <RiSendPlaneFill className="mr-2" /> Send email
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
